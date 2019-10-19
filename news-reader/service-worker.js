1375adeba8c9df830ecb79444303344e9fc8056d
Sofyan Thayf <sofyanthayf@gmail.com>
2019-10-19 16:29:11 +0800

    updated

--- news-reader/service-worker.js
+++ news-reader/service-worker.js
@@ -3,21 +3,13 @@ var urlsToCache = [
   "/",
   "/nav.html",
   "/index.html",
+  "/article.html",
   "/manifest.json",
-  "/favicon.ico",
   "/css/materialize.min.css",
-  "/img/android-chrome-192x192.png",
-  "/img/android-chrome-512x512.png",
-  "/img/mostdigital.png",
-  "/img/sofyanthayf.jpg",
   "/js/materialize.min.js",
   "/js/nav.js",
+  "/js/api.js",
   "/pages/home.html",
-  "/pages/team.html",
-  "/pages/contact.html",
-  "/pages/labs.html",
-  "/pages/class.html",
-  "/pages/links.html"
 ];
 
 self.addEventListener("install", function(event) {
@@ -29,22 +21,32 @@ self.addEventListener("install", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches
+  var base_url = "https://readerapi.codepolitan.com/";
+
+  if (event.request.url.indexOf(base_url) > -1) {
+    event.respondWith(
+      caches
       .match(event.request, { cacheName: CACHE_NAME })
       .then(function(response) {
         if (response) {
-          // console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
           return response;
         }
 
         console.log(
-          // "ServiceWorker: Memuat aset dari server: ",
+          "ServiceWorker: Memuat aset dari server: ",
           event.request.url
         );
         return fetch(event.request);
       })
-  );
+    );
+  } else {
+    event.respondWith(
+        caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+            return response || fetch (event.request);
+        })
+    )
+  }
 });
 
 
@@ -54,7 +56,7 @@ self.addEventListener("activate", function(event) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
           if (cacheName != CACHE_NAME) {
-            // console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
             return caches.delete(cacheName);
           }
         })
